a64490e09c1667654893274f7534e1b65945b418
Jasper H. Bojsen <liveowl@outlook.com>
2019-09-08 15:39:21 +0200

    Start

---
+++ src/Lauguage-Ext.Test/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using Lauguage_Ext.Test.Model;
+using Xamarin.Forms;
+
+namespace Lauguage_Ext.Test
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+        private void Test_OnClicked(object sender, EventArgs e)
+        {
+            // This makes the app crash most of the time. Why?
+            var objA = new ObjectA<object>(new object());
+        }
+    }
+}
---
+++ src/Lauguage-Ext.Test/Model/ObjectA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LanguageExt;
+
+namespace Lauguage_Ext.Test.Model
+{
+    public sealed class ObjectA<T> : Record<ObjectA<T>>
+    {
+        public T Obj { get; }
+
+        public ObjectA(T obj)
+        {
+            Obj = obj;
+        }
+    }
+}
